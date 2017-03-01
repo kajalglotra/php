28dacbe970554a333c0c9e7948c9d5c361348af0
kajalglotra <bulbulkjl@gmail.com>
2017-03-01 16:54:22 +0530

    Update logout.php

--- php_mysql/logout.php
+++ php_mysql/logout.php
@@ -1,7 +1,8 @@
 <?php
    session_start();
-   
-   if(session_destroy()) {
-      header("Location: login.php");
-   }
+   if(isset($_SESSION['login_user']))
+   {
+     unset($_SESSION['login_user']);
+    header("Location: login.php");
+    }
 ?>
