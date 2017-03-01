6605bef6c41d425c6b3375b917163529ccaec137
kajalglotra <bulbulkjl@gmail.com>
2017-03-01 16:52:28 +0530

    Update edit.php

--- php_mysql/edit.php
+++ php_mysql/edit.php
@@ -4,18 +4,31 @@
      $id=$_REQUEST['id'];
      $sql ="SELECT * FROM admin_account where id=$id";
      $result = mysqli_query($db,$sql);
+     if(isset($_POST['update'])){
+        $name=$_POST['username'];
+      $password=$_POST['password'];
+      $id1=$_POST['id'];
+ 
+      $sql ="UPDATE admin_account set username='$name' ,password='$password'  where id=$id1";
+       $result = mysqli_query($db,$sql);
+       header("location:userview.php");
+     }
 ?>
     <html>
 
     <head>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
 
     <body>
-        <form action="update.php" method="post">
+        <div class="container">
+        <form action="#" method="post">
+        <h1><center><u>Edit your info</u></center></h1>
             <table class="table table-bordered">
                 <?php
 	             $row = mysqli_fetch_array($result);
-                ?>
+                ?>  
+                     <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
                     <tr>
                         <td>Name:</td>
                         <td> <input type="text" name="username" value="<?php echo $row['username']; ?>"></td>
@@ -24,11 +37,9 @@
                         <td>Password:</td>
                         <td> <input type="text" name="password" value="<?php echo $row['password']; ?>"></td>
                     </tr>
-                     <tr>
-                        <td> <input type="hidden" name="id" value="<?php echo $row['id']; ?>"></td>
-                    </tr>
                     <tr>
-                        <td><input type="submit" value="submit">
+                        <td></td>
+                        <td><input type="submit" value="submit" name="update">
                         </td>
                     </tr>
             </table>
