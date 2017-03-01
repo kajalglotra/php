f37b5fbbbe4d81cf05eb2202cbbc1a375aed9ba8
kajalglotra <bulbulkjl@gmail.com>
2017-03-01 16:53:32 +0530

    Update login.php

--- php_mysql/login.php
+++ php_mysql/login.php
@@ -1,31 +1,34 @@
 <?php
+    $error='';
    include("config.php");
-   session_start();
-   $error='';
+    session_start();
+
+  if(isset($_SESSION['login_user'])){
+ header("location:userview.php");
+  }  if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password']) ) {
+    // username and password sent from form
    
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM admin_account WHERE username = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-      
-      if($count == 1) {
-         echo($myusername);
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: userview.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+   
+    $sql = "SELECT id FROM admin_account WHERE username = '$myusername' and password = '$mypassword'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+   
+    $count = mysqli_num_rows($result);
+   
+    // If result matched $myusername and $mypassword, table row must be 1 row
+   
+    if($count == 1) {
+       echo($myusername);
+       $_SESSION['login_user'] = $myusername;
+     
+       header("location: userview.php");
+    }else {
+       $error = "Your Login Name or Password is invalid";
+    }
+ }
+
 ?>
 <html>
    
