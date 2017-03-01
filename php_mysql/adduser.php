7c74b7817cdc28bef97819f4a1c51d837e8d33fc
kajalglotra <bulbulkjl@gmail.com>
2017-03-01 16:49:19 +0530

    Update adduser.php

--- php_mysql/adduser.php
+++ php_mysql/adduser.php
@@ -1,38 +1,52 @@
-
+<?php
+   include("config.php");
+  include('session.php');
+  if(isset($_POST['add'])){
+ 
+  $name=$_POST['username'];
+  $password=$_POST['password'];
+  $sql="INSERT INTO admin_account
+  (username,password) VALUES ('$name','$password')";
+  $result = mysqli_query($db,$sql);
+  header("location:userview.php");
+}
+?>
 <html>
+
 <head>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
+
 <body>
-	
-	<form action="insert.php" method="POST" >
+     <div class="container">
+     <center> <h1> Add Another User</h1></center>
+      <form action="#" method="POST">
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td> <input type="text" name="username" value=""></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td> <input type="text" name="password" value=" "></td>
+            </tr>
 
-<table>
-	
-	<tr>
-	<td>Name:</td>
-	<td> <input type="text" name="username" value=""></td>
-	</tr>
-	<tr>
-	<td>Password</td>
-	<td> <input type="text" name="password" value=" "></td>
-	</tr>
-	
-	<tr>
-		<td><input type="submit" value="submit">
-        </td>
-	</tr>
-	</table>
+            <tr>
+                <td></td>
+                <td><input type="submit" class="btn-info" value="submit" name="add">
+                </td>
+            </tr>
+        </table>
 
 
 
-</form>
-	
-	
-	
-	
-	</div>
-	</div>
+    </form>
 
-</div>
+
+
+
+    </div>
+   
 </body>
+
 </html>
