a11ec4004ac8150de1552d6c43cc3c267fb3ae65
kajalglotra <bulbulkjl@gmail.com>
2017-03-06 16:35:41 +0530

    Update userview.php

--- ajax/userview.php
+++ ajax/userview.php
@@ -1,4 +1,15 @@
- <html>
+ <?php
+include("config.php");
+$result=mysqli_query($db,"select * from admin_account");
+      if(isset($_GET['action'] ) &&  $_GET['action'] =='delete'){
+       
+        $id=$_REQUEST['id'];
+        $sql ="DELETE from  admin_account  where id=$id";
+         mysqli_query($db,$sql);
+     //    header("location:userview.php");
+      }
+      ?>
+    <html>
 
     <head>
         <link rel="stylesheet" href="style.css">
@@ -7,6 +18,11 @@
                 color: black;
                 font-size: 15;
             }
+            
+            table th {
+                text-align:center;
+            }
+        
         </style>
     </head>
 
@@ -21,23 +37,35 @@
                 <a href="adduser.php"><img src="add.png" width="30px" height="30px"></a>
                 <a href="logout.php"><img src="log.jpeg" width="30px" height="30px"></a>
             </div>
-            <div id="content"></div>
+            <div id="content">
+                <table class='table'>
+                    <tr class="info">
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Status</th>
+                        <th>Id</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </table>
             </div>
-            </body>
-            <script src="jquery.js"></script>
-             <script>
-            $(document).ready(function(){
+        </div>
+    </body>
+    <script src="jquery.js"></script>
+    <script>
+        $(document).ready(function() {
             var response = '';
-            $.ajax({ type: "GET",   
-                     url: "display.php",   
-                     async: false,
-                     success : function(text)
-                     {
-                         response = text;
-                         reply="<table  class='table table-bordered'><tr class='danger'>";
-                          $("#content").html(reply+response);
-                     }
+            $.ajax({
+                type: "GET",
+                url: "display.php",
+                async: false,
+                success: function(text) {
+                    response = text;
+                    reply = "<table class='table'>";
+                    $("#content").append(reply + response);
+                }
             });
-            });
-            </script>
+        });
+    </script>
+
     </html>
