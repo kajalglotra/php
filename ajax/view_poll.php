ee88a2c8e3fe11d560c83680779b2d6ae0ebfc6e
kajalglotra <bulbulkjl@gmail.com>
2017-03-06 11:19:16 +0530

    Update view_poll.php

--- ajax/view_poll.php
+++ ajax/view_poll.php
@@ -13,34 +13,7 @@
                 font-size: 15;
             }
         </style>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-         <script type="text/javascript">
-          $(function() {
-             $(".delete").click(function(){
-                   var element = $(this);
-                   var del_id = element.attr("id");
-                   var tr = $(this).parents(".info");
-                  if(confirm("Are you sure you want to delete this?"))
-                   {
-                       $.ajax({
-                    type: "POST",
-                    url:"delete.php",
-                     data:{id:del_id},
-                      success: function(result){
-                        if(result == '1'){
-                        tr.animate({ backgroundColor: "#003" }, "slow")
-                        .animate({ opacity: "hide" }, "slow");
-                      }
-                      else{
-                        console.log(result);
-                      }
-                      }
-                    });
-
-                       }
-                });
-               });
-</script>
+       
     
     </head>
 
@@ -117,5 +90,32 @@
             ?>
         </div>
     </body>
+      <script src="jquery.js"></script>
+         <script type="text/javascript">
+          $(function() {
+             $(".delete").click(function(){
+                   var element = $(this);
+                   var del_id = element.attr("id");
+                   var tr = $(this).parents(".info");
+                  if(confirm("Are you sure you want to delete this?"))
+                   {
+                       $.ajax({
+                    type: "POST",
+                    url:"delete.php",
+                     data:{id:del_id},
+                      success: function(result){
+                        if(result == '1'){
+                        tr.animate({ backgroundColor: "#003" }, "slow")
+                        .animate({ opacity: "hide" }, "slow");
+                      }
+                      else{
+                        console.log(result);
+                      }
+                      }
+                    });
 
+                       }
+                });
+               });
+</script>
     </html>
